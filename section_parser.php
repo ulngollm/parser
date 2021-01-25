3cc485c66fffc88b36bc372fe9b6360f6a0f499c
ulngollm <ul@noknok.ru>
2021-01-25 17:37:59 +0300

    добавлена обработка страниц, корректно проходит выгрузка всего списка элементов

--- section_parser.php
+++ section_parser.php
@@ -1,5 +1,4 @@
 <?php
-include_once('detail.php');
 $file = file_get_contents('https://andimart.ru/catalog/');
 $domain = 'https://andimart.ru';
 // file_put_contents('page.html', $file);
@@ -7,64 +6,57 @@ $html = new DOMDocument();
 libxml_use_internal_errors(true);
 // $html->loadHTMLFile('page.html');
 $html->loadHTML($file);
-
-// var_dump($html);
 $parser = new DOMXPath($html);
-// $sections = $parser->query("//ul[@class='nav__list']//a[@class='sub-nav__child-link']"); все разделы
+
 $sections = $parser->query("//ul[@class='nav__list']/li[position()<4]//ul[@class='nav__child-group']/li[position()>1]/a");
 $section_links = array();
-
 foreach ($sections as $sect) {
     $section = array();
     $section['name'] = trim($sect->nodeValue);
     $section['url'] = $domain . $sect->attributes->getNamedItem('href')->nodeValue;
+    $section['elements'] = array();
     array_push($section_links, $section);
 }
 //перейти на страницу раздела
-//выбрать все элементы и запихать в массив elements()
-foreach ($section_links as $section) {
+// //выбрать все элементы и запихать в массив elements()
+echo memory_get_usage(true) / 1024;
+foreach ($section_links as &$section) {
     $elements = array();
-    $base_url =
-        get_offers_list($section['url'], $elements);
+    $base_url = $section['url'];
+    get_offers_list($base_url, $elements);
     $section['elements'] = $elements;
 }
- //че делать с пагинацией
-function get_offers_list(string $url, array &$list, bool $pagination = false, int $page = 1, int $page_count = 1)
+print_r($section_links);
+echo memory_get_peak_usage(true) / 1024;
+// $data = json_encode($section_links);
+// file_put_contents('structure.json', $data);
+
+//че делать с пагинацией
+function get_offers_list(string $url, array &$list,  int $page = 1)
 {
-    $file = ($page == 1) ? file_get_contents($url) : file_get_contents($url . "/?PAGEN_1=" . $page);
+    $page_url = "$url?PAGEN_1=$page";
+    print("$page_url\n");
+    $file = file_get_contents($page_url);
     $elem_page = new DOMDocument();
     $elem_page->loadHTML($file);
     $xpath = new DOMXPath($elem_page);
-
     $offers = $xpath->query("//div[@class='catalog__title']/a/@href");
     foreach ($offers as $offer) {
         $elem = $offer->nodeValue;
         array_push($list, $elem);
     }
     if ($page == 1) {
-        $pages = $xpath->query('//ul[@class="pagination__list"]/li[last()]/a/span/text()');
-        if ($pages->length > 1) {
-            $count = $pages->item(0)->nodeValue;
-            get_offers_list($url, $elements, true, $page++, $count);
+        $total_elem_count = $xpath->query("//span[@class='count__val']");
+        $total_elem_count = $total_elem_count->item(0)->nodeValue;
+        print("$total_elem_count\n");
+        $elem_count =  $offers->length;
+        if ($total_elem_count > $elem_count) {
+            $pages_count = ceil($total_elem_count / $elem_count);
+            for ($i = 2; $i <= $pages_count; $i++) {
+                get_offers_list($url, $list, $i);
+            }
         }
     }
-    if ($pagination && $page <= $page_count) {
-        get_offers_list($url);
-    }
 }
-print_r($section_links);
 
-// $xml = new DOMDocument('1.0', 'utf-8');
-// $xml->formatOutput = true;
-// $root = $xml->createElement('offers');
-// $xml->appendChild($root);
-// foreach($category_list as $url){
-//     print($url);
-//     $offer = new Offer($url, $xml);
-//     $offer_xml = $offer->get_xml();
-//     $root->appendChild($offer_xml);
 
-    
-// }
-
-// $xml->save('offers.xml');
