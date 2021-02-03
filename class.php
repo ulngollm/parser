83774de33c6f2c9c650686c03a62ee73755c2166
ulngollm <ul@noknok.ru>
2021-02-03 18:29:18 +0300

    add section parser as child of parser

--- class.php
+++ class.php
@@ -1,13 +1,6 @@
 <?php
-//получить корневые разделы
-// $sections = $parser->query('//div[@id="dropdown_5106"]/ul/li[position()>1 and position() <10]/a[@href]');
-// foreach($sections as $section){
-//     $url = $parser->query('./@href', $section);
-//     file_get_contents($section);
-// }
-
-
-class Parser{
+class Parser
+{
     public DOMXPath $parser;
 
     function __construct($url)
@@ -18,17 +11,52 @@ class Parser{
         $html->loadHTML($page);
         $this->parser = new DOMXPath($html);
     }
-    private function get_page($url){
+    private function get_page($url)
+    {
         $id = curl_init($url);
         curl_setopt($id, CURLOPT_RETURNTRANSFER, 1);
         $page = curl_exec($id);
         curl_close($id);
         return $page;
     }
-    public function query(string $query){
-        return $this->parser->query($query);
-    }
-    public function get_elements(){
-        //получить их прямо в массив
+    public function query(string $query, $contextNode = null)
+    {
+        return $this->parser->query($query, $contextNode);
     }
 }
+class SectionParser extends Parser
+{
+    public function get_parent_sections()
+    {
+        $section_list = array();
+        $sections = $this->parser->query('//div[@id="dropdown_5106"]/ul/li[position()>1 and position() <10]/a[@href]');
+        foreach ($sections as $section) {
+            $link = $this->parser->query('./@href', $section)->item(0)->nodeValue;
+            $name = $this->parser->query('./text()', $section)->item(0)->nodeValue;
+            $code = md5($name);
+            $section_item = array('name' => $name, 'code' => $code, 'link' => $link);
+            array_push($section_list, $section_item);
+        }
+        return $section_list; //массив с названиями разделов и ссылками
+    }
+
+    public function get_subsections()
+    {
+        //ващет мы сначала должны попасть на страницу
+        $section_list = array();
+        $sections = $this->parser->query('//li[@class="ty-subcategories__item"]');
+        foreach ($sections as $section) {
+            $name;
+            $code;
+            $parent_code;
+        }
+    }
+}
+
+// $section_parser = new Parser('https://daichi-aircon.ru/');
+// $sections = $section_parser->query('//div[@id="dropdown_5106"]/ul/li[position()>1 and position() <10]/a[@href]');
+// foreach($sections as $section){
+//     $url = $section_parser->parser->query('./@href', $section)->item(0)->nodeValue;
+//     $name = $section_parser->query('./text()', $section)->item(0)->nodeValue;
+//     print("$name \t $url\n");
+// }
