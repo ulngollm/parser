2e03f0d53bb714836122d4d85cf8683de8909ae0
ulngollm <ul@noknok.ru>
2021-02-04 21:13:05 +0300

    исправлены ошибки, добавлены пояснения, генерация xml из разделов

--- parser.php
+++ parser.php
@@ -1,9 +1,8 @@
 <?php
 include_once('./classes/class.php');
+include_once('./classes/xml.php');
 
 $url = 'https://daichi-aircon.ru/';
-$parser = new SectionParser($url, $section_params);
-// $xml = new XMLGenerator();
 
 $section_params = array(
     'section' => '//div[@id="dropdown_5106"]/ul/li[position()>1 and position() <10]/a[@href]',
@@ -18,37 +17,52 @@ $subsection_params = array(
 );
 $detail_params = array();
 
-$sections = $parser->get_section_list($section_params);
-print_r($sections);
+$parser = new SectionParser($url, $section_params);
+$xml = new XMLGenerator();
+
+//получаем список верхних разделов
+$root_sections = $parser->get_section_list();
+// print_r($root_sections);
 unset($parser);
 
 $section_list = array();
-foreach ($sections as &$section) {
+//проходимся по страницам верхних разделов
+foreach ($root_sections as &$section) {
+    $xml->add_category($section);
     $subparser = new SectionParser($section['link'], $subsection_params, $section['code']);
+    //смотрим, есть ли товары на этой странице
+    $elements = $subparser->get_elements_list();
+    if ($elements) $section['elements']  = $elements;
+    
+    //проверяем, есть ли у раздела вложенные
+    //если раздел отмечен в меню как текущий, значит, вложенных нет
     $isCurrent = $subparser->query('//li[@class="ty-subcategories__current_item"]')->length; 
-
-    if (!$isCurrent) {//если у раздела нет вложенных
-        echo 1;
+    if (!$isCurrent) {
         $subsections = $subparser->get_section_list();
-        $elements = $subparser->get_elements_list();
-        if ($elements) $section['elements']  = $elements;
         $section_list = array_merge($section_list, $subsections);
-    }
-    else {
-        $elements = $subparser->get_elements_list();
-        if ($elements) $section['elements']  = $elements;
     }
 }
 
+//проходим по страницам вложенных разделов
 foreach($section_list as &$section){
+    $xml->add_category($section);
+    //если это не те верхние разделы, в которых были элементы, получаем ссылки товаров
     if(!isset($section['elements'])){
         $subparser = new SectionParser($section['link'], $subsection_params, $section['code']);
         $section['elements'] = $subparser->get_elements_list();
     }
 }
-print_r(count($section_list));
-save_json(array_merge($sections, $section_list));
+$xml->xml->save('output/xml.xml');
 
+print_r(count($section_list));
+// save_json(array_merge($root_sections, $section_list));//debug
+
+//пойти по всем товарам
+foreach($section_list['elements'] as $elem){
+    $offer = new Offer($elem, $parent, $params);
+}
+
+$xml->xml->save('output/xml.xml');
 
 function save_json(array $arr)
 {
