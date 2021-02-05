120411bead7952a3d64d6703065733369aaa73f7
ulngollm <ul@noknok.ru>
2021-02-05 19:15:11 +0300

    последний раздел не грузится, тесты

--- test/detail_test.php
+++ test/detail_test.php
@@ -7,7 +7,7 @@ include_once('./classes/detail.php');
 $params = array(
     'name' => '//h1[@class="ty-product-block-title"]/bdi/text()',
     'images' => '//div[contains(@id, "product_images")]/div/div[@class="ty-product-img cm-preview-wrapper"]/a/@href',
-    'desc'=>'//div[@id="content_description"]/div',
+    'desc'=>'//div[@id="content_description"]/div/p[text()]',
     'props'=>'//div[@id="content_features"]//div[@class="ty-product-feature"]',
     'price'=>'//div[@class="ty-product-block__price-actual"]//span[@class="ty-price-num"][1]',
     'article'=>'//div[@class="ty-product-block__sku"]/div/span',
@@ -17,9 +17,9 @@ $parent_code = 'gjvjgchadasco';
 
 $url = "https://daichi-aircon.ru/nastennye-split-sistemy-daichi/daichi-da25evq1-df25ev1/";
 $offer = new Offer($url, $parent_code, $params);
-print_r($offer);
+print_r($offer->description);
 
-$xml = new XMLGenerator();
-$xml->add_offer($offer);
-$xml->xml->save('./output/one.xml');
+// $xml = new XMLGenerator();
+// $xml->add_offer($offer);
+// $xml->xml->save('./output/one.xml');
 
