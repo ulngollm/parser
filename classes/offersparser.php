1fd9fa02c45a3898f0f02cd1fb7c7a64b2a1053b
ulngollm <ul@noknok.ru>
2021-05-11 20:33:10 +0300

    delete removed constructor params, some refactoring of OffersParser

--- classes/offersparser.php
+++ classes/offersparser.php
@@ -1,18 +1,19 @@
 <?php
 class OffersParser extends SectionParser
+//запихала логику для единичного случая в целый класс. Класс одноразовый, для переиспользования переписать
 {
     public array $elements_list;
+    const OFFER_TYPE = ['simple', 'complex', 'offer'];
 
-    public function __construct(string $url, array $params, array $elements_list, string $parent_code = null)
+    public function __construct(string $url, array &$elements_list, string $parent_code = null)
     {
-        parent::__construct($url, $params, $parent_code);
-        $this->elements_list = &$elements_list; 
+        parent::__construct($url, $parent_code);
+        $this->elements_list = $elements_list; //оставляю, потому что обращение к elements_list есть в 5 методах. Не кайф передавать этот массив из метода в метод
     }
-
-    public function get_elements_list(string $elements_xpath = null): ?array
+    public function get_elements_list(array $xpath): ?array
     {
-        $xpath = $this->xpath;
-        $elements_id = $this->parser->query($xpath['element_id']);
+
+        $elements_id = $this->query($xpath['id']);
         if ($elements_id->length) {
             foreach ($elements_id as $elem_id) {
                 $bx_id = $elem_id->nodeValue;
@@ -22,7 +23,7 @@ class OffersParser extends SectionParser
                     $this->add_section_link($id);
                     print('duble');
                 } else {
-                    $elem = $this->parser->query('./..', $elem_id)->item(0);
+                    $elem = $this->query('./..', $elem_id)->item(0);
                     $this->add_new_element($elem, $id);
                     print('elem');
 
@@ -38,18 +39,20 @@ class OffersParser extends SectionParser
     {
         return array_key_exists($id, $this->elements_list);
     }
+
     private function add_section_link($id)
     {    
         $section_list = &$this->elements_list[$id]['section'];
         if(!in_array($this->parent_code, $section_list))
             array_push($section_list, $this->parent_code);
     }
+
     public function add_new_element(DOMNode $elem, $id)
     {
-        $link = $this->parser->query($this->xpath['link'], $elem)->item(0)->nodeValue;
-        $name = $this->parser->query($this->xpath['name'], $elem)->item(0)->nodeValue;
+        $link = $this->query($this->xpath['link'], $elem)->item(0)->nodeValue;
+        $name = $this->query($this->xpath['name'], $elem)->item(0)->nodeValue;
         if($this->exist_exclude_brand($name)) return false; //не добавлять исключенный бренд
-        $isSimple = $this->parser->query($this->xpath['class_single'], $elem)->length;
+        $isSimple = $this->query($this->xpath['class_single'], $elem)->length;
         $type = $isSimple ? "0" : '1'; //simple of complex
         $this->elements_list[$id] = array(
             'section' => array($this->parent_code),
