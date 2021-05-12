903a68005001ed2112991a5ad4b873c80974232a
ulngollm <ul@noknok.ru>
2021-05-12 16:40:11 +0300

    add recursive to section parser

--- classes/sectionparser.php
+++ classes/sectionparser.php
@@ -1,7 +1,7 @@
 <?php
 class SectionParser extends Parser
 {
-    public string $parent_code;
+    public ?string $parent_code;
 
     public function __construct(string $url, ?string $parent_code = null)
     {
@@ -15,16 +15,20 @@ class SectionParser extends Parser
         foreach ($sections as $section) {
             $section_item = $this->get_one_section_data($section, $xpath);
             self::add_sections($section_list, $section_item);
+
             if($recursive){
-                $subsection_parser = new SectionParser($section_item['link'], $section_item['code']);
+                $url = $this->prepare_section_link($section_item['link']);
+                $parent_code = $section_item['code'];
+                $subsection_parser = new SectionParser($url, $parent_code);
                 $subsection_parser->get_section_list($xpath, $section_list, $recursive);
+                //а что делать, если рекурсивно и элементы собрать?
             }
         }
     }
-
+    
     private function get_one_section_data($section, $xpath){
         $link =$this->parse_single_value($xpath['link'], $section);
-        $name = trim($this->parse_single_value($xpath['name'], $section));
+        $name = $this->parse_single_value($xpath['name'], $section);
         $code = md5($name . $this->parent_code);
         //добаить картинку?
         $section_data = array(
@@ -41,6 +45,10 @@ class SectionParser extends Parser
         array_push($arr, ...$sections);
     }
 
+    protected function prepare_section_link($link){
+        return !self::is_relative_link($link)? $link : $this->base_url.$link;
+    }
+    
     public function get_elements_links(string $xpath, array &$element_links)
     {
         $links = $this->query($xpath);
