d249fa66a21b816574ae302b66e12429659a78a5
ulngollm <ulngollm@gmail.com>
2021-05-13 12:33:35 +0300

    complete offers parser

--- classes/offersparser.php
+++ classes/offersparser.php
@@ -1,32 +1,33 @@
 <?php
+class OfferType {
+    const SIMPLE = 0;
+    const COMPLEX = 1;
+    const OFFER = 2;
+}
 class OffersParser extends SectionParser
-//запихала логику для единичного случая в целый класс. Класс одноразовый, для переиспользования переписать
 {
     public array $elements;
-    const OFFER_TYPE = ['simple', 'complex', 'offer'];
-
     public function __construct(string $url, array &$elements, string $section_code = null)
     {
         parent::__construct($url, $section_code);
         $this->elements = &$elements;
     }
-    public function get_elements_list(array $xpath, callable $callback = null)
+    public function get_elements_list(array $xpath, callable $get_type = null)
     {
         $elements = $this->query($xpath['item']);
         foreach ($elements as $element) {
-            $offer = $this->get_one_element_data($element, $xpath, $callback);
+            $offer = $this->get_one_element_data($element, $xpath, $get_type);
             $this->add_offer($offer);
         }
     }
-    private function get_one_element_data(DOMNode $element, array $xpath, $callback = null)
+    private function get_one_element_data(DOMNode $element, array $xpath, callable $get_type = null)
     {
         $id = $this->parse_single_value($xpath['id'], $element);
         self::prepare_id($id);
         $name = $this->parse_single_value($xpath['name'], $element);
-        $link = $this->parse_single_value($xpath['link'], $element);
-        
-        //todo: опредеить, что товар сложный
-        $type = null;
+        $link = $this->parse_single_value($xpath['link'], $element);        
+        $type = $get_type($this, $element, $xpath['class']);
+
         $element_data = array(
             'id' => $id,
             'type' => $type,
@@ -36,24 +37,27 @@ class OffersParser extends SectionParser
         );
         return $element_data;
     }
-    public static function prepare_id(&$id){
-        $tmp_id = explode('_', $id);
-        $id = end($tmp_id);
-    }
 
     public function add_offer($element)
     {
         $id = $element['id'];
         $name = $element['name'];
         if ($this->elem_exist($id)) $this->add_section_link($id);
-        elseif (!self::exist_exclude_brand($name)) $this->elements[$id] = $element;
+        elseif (!self::exist_exclude_brand($name)) $this->add_new_elem($element);
+    }
+
+    public static function prepare_id(&$id)
+    {
+        $tmp_id = explode('_', $id);
+        $id = end($tmp_id);
     }
 
     public function elem_exist($id)
     {
         return array_key_exists($id, $this->elements);
     }
-    public static function exist_exclude_brand($name, $brand = null)
+    //метод только для этого парсера. Потом переделать
+    public static function exist_exclude_brand($name)
     {
         return (bool) stripos($name, 'guardo');
     }
@@ -65,4 +69,8 @@ class OffersParser extends SectionParser
             array_push($section_links, $this->section_code);
     }
 
+    private function add_new_elem($element){
+        $id = $element['id'];
+        $this->elements[$id] = $element;
+    }
 }
--- test/offers.php
+++ test/offers.php
@@ -3,8 +3,9 @@ const ROOT = '/home/ully/Документы/parser/catalog_parser';
 include_once(ROOT . '/utils/autoload.php');
 include_once(ROOT . '/utils/stat.php');
 
-$url = 'https://www.home-heat.ru/catalog/panelnye-radiatory-s-gorizontalnymi-nasechkami/';
-// $url = 'https://www.home-heat.ru/catalog/trubchatye-radiatory-guardo-retta-6p-s-pryamougolnym-profilem/';
+// $url = 'https://www.home-heat.ru/catalog/panelnye-radiatory-s-gorizontalnymi-nasechkami/';//раздел с 4 товарами, удобно для теста
+// $url = 'https://www.home-heat.ru/catalog/trubchatye-radiatory-guardo-retta-6p-s-pryamougolnym-profilem/';// с товарами guardo - проверка исключеия бренда
+$url = 'https://www.home-heat.ru/catalog/vse-vertikalnye-radiatory/?PAGEN_1=7';//c спростыми товарами
 $elements = array();
 $xpath = array(
     'item'=>'//div[contains(@class,"pr_list-item")]',
@@ -15,13 +16,15 @@ $xpath = array(
     'next_page' => '//ul[@class="pagination"]/li[@class="ax-pag-next"]/a/@href'
 );
 $exclude_brand = 'guardo';
-$parser = new OffersParser($url,$elements);
-$parser->get_elements_list($xpath);
+$parser = new OffersParser($url,$elements, 'aascujli36');
+$parser->get_elements_list($xpath,'get_offer_type');
 print_r($elements);
 
 
-
-// function get_offer_type($parser, $element, $xpath = null){
-//     $class_attr = $parser->parse_single_value($xpath['class'], $element);//парсим класс
-//     print_r($class_attr);
-// }
+//callback для определения типа товара
+//можно встроить любую свою фукнцию
+function get_offer_type(Parser $parser, DOMNode $element, string $xpath){
+    $class_attr = $parser->parse_single_value($xpath, $element);//парсим класс
+    if(strpos($class_attr, 'single-product')) return OfferType::SIMPLE;
+    else return OfferType::COMPLEX;
+}
