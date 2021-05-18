92c0e00eef36a8160e88e38325318f17049dd746
ulngollm <ul@noknok.ru>
2021-05-18 18:42:41 +0300

    update with old method with offer object

--- classes/xmlgenerator.php
+++ classes/xmlgenerator.php
@@ -9,96 +9,27 @@ class XMLGenerator
     {
         $this->xml = new DOMDocument('1.0', 'utf-8');
         $this->xml->formatOutput = true;
-        $root = $this->xml->createElement('catalog');
-        $this->xml->appendChild($root);
-        $this->categories = $this->xml->createElement('categories');
-        $root->appendChild($this->categories);
-        $this->offers = $this->xml->createElement('offers');
-        $root->appendChild($this->offers);
+
+        $root = $this->add_elem('catalog', null, $this->xml);
+        $this->categories = $this->add_elem('catalog', null, $root);
+        $this->offers = $this->add_elem('offers', null, $root);
     }
-    public function add_category(array $section)
+
+    private function add_elem(string $node_name, ?string $value, DOMNode $parent_node)
     {
-        $category = $this->xml->createElement('category', $section['name']);
-        $this->categories->appendChild($category);
-        $category_code = $this->xml->createAttribute('id');
-        $category_code->value = $section['code'];
-        $category->appendChild($category_code);
-        if (isset($section['parent_code'])) {
-            $parent_category_code = $this->xml->createAttribute('parentId');
-            $parent_category_code->value = $section['parent_code'];
-            $category->appendChild($parent_category_code);
-        }
+        $elem = $this->xml->createElement($node_name, $value);
+        $parent_node->appendChild($elem);
+        return $elem;
     }
-    public function add_offer(Offer $offer)
+
+    private function add_attr(string $attr_name, string $value, DOMNode $node)
     {
-        $offer_elem = $this->xml->createElement('offer');
-        $name = $this->xml->createElement('name', $offer->name);
-        $price = $this->xml->createElement('price', $offer->price);
-        $code = $this->xml->createElement('code', $offer->article);
-        if ($offer->description) {
-            $description = $this->xml->createElement('desc');
-            $description_html = $this->xml->createCDATASection($offer->description);
-            $offer_elem->appendChild($description);
-            $description->appendChild($description_html);
-        }
-        if ($offer->preview_desc) {
-            $preview = $this->xml->createElement('preview_text');
-            $preview_html = $this->xml->createCDATASection($offer->preview_desc);
-            $offer_elem->appendChild($preview);
-            $preview->appendChild($preview_html);
-        }
-        $brand = $this->xml->createElement('brand', $offer->brand);
-        $images = $this->xml->createElement('images');
-        $properties = $this->xml->createElement('props');
-        $category = $this->xml->createElement('category', $offer->category_code ? $offer->category_code : $offer->section_path);
-
-        $this->offers->appendChild($offer_elem);
-        $offer_elem->appendChild($name);
-        $offer_elem->appendChild($category);
-        $offer_elem->appendChild($code);
-        $offer_elem->appendChild($price);
-        if (isset($offer->brand))   $offer_elem->appendChild($brand);
-        $offer_elem->appendChild($images);
-        $offer_elem->appendChild($properties);
-
-        foreach ($offer->images as $image) {
-            $img = $this->xml->createElement('image', $image);
-            $images->appendChild($img);
-        }
-        $this->add_props($offer->properties, $properties);
+        $attr = $this->xml->createAttribute($attr_name);
+        $attr->value = $value;
+        $node->appendChild($attr);
     }
 
-    public function add_props(array $props, &$properties)
-    {
-        foreach ($props as $property) {
-            $name = trim($property[0], ":");
-            $value = $property[1];
-            $code = md5($name);
-            $prop = $this->xml->createElement('prop');
-            $propName = $this->xml->createElement('name', $name);
-            $propCode = $this->xml->createAttribute('id');
-            $propCode->value = $code;
-            $propValue = $this->xml->createElement('value', $value);
-            $prop->appendChild($propName);
-            $prop->appendChild($propCode);
-            $prop->appendChild($propValue);
-            $properties->appendChild($prop);
-        }
-    }
-
-    public function offers_from_array(array $elements)
-    {
-        foreach ($elements as $id => $obj) {
-            $offer = $this->create_offer();
-
-            foreach ($obj as $key => $value) {
-                if ($key != 'id' || $key != 'model')
-                    $this->add_elem($key, $value, $offer);
-                else $this->add_attr($key, $value, $offer);
-            }
-        }
-    }
-    public function add_html(string $node_name, string $html, DOMElement $parent_node)
+    private function add_html(string $node_name, string $html, DOMNode $parent_node)
     {
         $elem = $this->xml->createElement($node_name);
         $content = $this->xml->createCDATASection($html);
@@ -106,25 +37,105 @@ class XMLGenerator
         $parent_node->appendChild($elem);
     }
 
-    public function add_attr(string $attr_name,string $value, DOMElement $node)
+    public function add_offer(Offer $offer)
     {
-        $attr = $this->xml->createAttribute($attr_name);
-        $attr->value = $value;
-        $node->append($attr);
+        $elem = $this->create_offer();
+        $this->add_elem('price', $offer->price, $elem);
+        $this->add_elem('code', $offer->article, $elem);
+        if ($offer->description) $this->add_html('desc', $offer->description, $elem);
+        if ($offer->preview_desc) {
+            $this->add_html('preview_text', $offer->preview_desc, $elem);
+        }
+        $this->add_elem('brand', $offer->brand, $elem);
+        $this->add_images($offer->images, $elem);
+        $this->add_props($offer->properties, $elem);
+        $this->add_category_prop($offer->category_code, $elem);
     }
 
-    public function add_elem(string $node_name, string $value, DOMElement $parent_node)
+    public function convert_from_json(string $filename)
     {
-        $elem = $this->xml->createElement($node_name, $value);
-        $parent_node->appendChild($elem);
+        $catalog = Utils::load_from_json($filename, false);
+        foreach($catalog['category'] as $category)
+            $this->add_category($category);
+        foreach($catalog['offers'] as $offer)
+            $this->add_offer_from_array($offer);
+    }
+    
+    public function add_category(array $section)
+    {
+        $category = $this->add_elem('category', $section['name'], $this->categories);
+        $this->add_attr('id', $section['code'], $category);
+        if (isset($section['parent_code'])) {
+            $this->add_attr('parentId', $section['parent_code'], $category);
+        }
+    }
+    public function add_offer_from_array(array $offer_data)
+    {
+        $offer = $this->create_offer();
+        foreach ($offer_data as $param => $data) {
+            switch ($param) {
+                case 'id': 
+                case 'model': 
+                case 'type':
+                    $this->add_attr($param, $data, $offer);
+                    break;
+                case 'desc':
+                case 'preview':
+                    $this->add_html($param, $data, $offer);
+                    break;
+                case 'section':
+                    $this->add_category_prop($data, $offer);
+                    break;
+                case 'images':
+                case 'props': 
+                    $method_name = "add_$param";
+                    $this->$method_name($data, $offer);
+                    break;
+                default:
+                    $this->add_elem($param, $data, $offer);
+                    break;
+            }
+        } 
     }
 
     private function create_offer()
     {
         $offer = $this->xml->createElement('offer');
-        $this->offers->append($offer);
+        $this->offers->appendChild($offer);
         return $offer;
     }
+
+    private function add_images(array $images, DOMNode $offer)
+    {
+        $img = $this->add_elem('images', null, $offer);
+        foreach ($images as $image) {
+            $this->add_elem('image', $image, $img);
+        }
+    }
+
+    public function add_props(array $props, DOMNode &$offer)
+    {
+        $properties = $this->add_elem('props', null, $offer);
+        foreach ($props as $id => $property) {
+            $this->add_single_property($property, $id, $properties);
+        }
+    }
+
+    private function add_single_property(array $property, string $id,  DOMNode &$properties)
+    {
+        $prop = $this->add_elem('property', $property['value'], $properties);
+        $this->add_attr('id', $id, $prop);
+        $this->add_attr('name', $property['name'], $prop);
+    }
+
+    public function add_category_prop(array|string $sections, DOMNode &$offer){
+        $value = implode(';', $sections);
+        $this->add_elem('category', $value, $offer);
+    }
+
+    
+
+    
 
     public function save_xml(string $filename)
     {
