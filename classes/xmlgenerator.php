f817a08c7f42fb2d86323cc37b5c009022a494e9
ulngollm <ul@noknok.ru>
2021-05-18 18:48:20 +0300

    remove unused, update constructor

--- classes/xmlgenerator.php
+++ classes/xmlgenerator.php
@@ -5,7 +5,7 @@ class XMLGenerator
     private DOMElement $categories;
     private DOMElement $offers;
 
-    public function __construct()
+    public function __construct(?string $filename = null)
     {
         $this->xml = new DOMDocument('1.0', 'utf-8');
         $this->xml->formatOutput = true;
@@ -13,6 +13,7 @@ class XMLGenerator
         $root = $this->add_elem('catalog', null, $this->xml);
         $this->categories = $this->add_elem('catalog', null, $root);
         $this->offers = $this->add_elem('offers', null, $root);
+        if ($filename) $this->convert_from_json($filename);
     }
 
     private function add_elem(string $node_name, ?string $value, DOMNode $parent_node)
@@ -37,30 +38,15 @@ class XMLGenerator
         $parent_node->appendChild($elem);
     }
 
-    public function add_offer(Offer $offer)
-    {
-        $elem = $this->create_offer();
-        $this->add_elem('price', $offer->price, $elem);
-        $this->add_elem('code', $offer->article, $elem);
-        if ($offer->description) $this->add_html('desc', $offer->description, $elem);
-        if ($offer->preview_desc) {
-            $this->add_html('preview_text', $offer->preview_desc, $elem);
-        }
-        $this->add_elem('brand', $offer->brand, $elem);
-        $this->add_images($offer->images, $elem);
-        $this->add_props($offer->properties, $elem);
-        $this->add_category_prop($offer->category_code, $elem);
-    }
-
     public function convert_from_json(string $filename)
     {
         $catalog = Utils::load_from_json($filename, false);
-        foreach($catalog['category'] as $category)
+        foreach ($catalog['category'] as $category)
             $this->add_category($category);
-        foreach($catalog['offers'] as $offer)
+        foreach ($catalog['offers'] as $offer)
             $this->add_offer_from_array($offer);
     }
-    
+
     public function add_category(array $section)
     {
         $category = $this->add_elem('category', $section['name'], $this->categories);
@@ -74,8 +60,8 @@ class XMLGenerator
         $offer = $this->create_offer();
         foreach ($offer_data as $param => $data) {
             switch ($param) {
-                case 'id': 
-                case 'model': 
+                case 'id':
+                case 'model':
                 case 'type':
                     $this->add_attr($param, $data, $offer);
                     break;
@@ -87,7 +73,7 @@ class XMLGenerator
                     $this->add_category_prop($data, $offer);
                     break;
                 case 'images':
-                case 'props': 
+                case 'props':
                     $method_name = "add_$param";
                     $this->$method_name($data, $offer);
                     break;
@@ -95,7 +81,7 @@ class XMLGenerator
                     $this->add_elem($param, $data, $offer);
                     break;
             }
-        } 
+        }
     }
 
     private function create_offer()
@@ -103,6 +89,12 @@ class XMLGenerator
         $offer = $this->xml->createElement('offer');
         $this->offers->appendChild($offer);
         return $offer;
+    }
+
+    private function add_category_prop(array|string $sections, DOMNode &$offer)
+    {
+        $value = implode(';', $sections);
+        $this->add_elem('category', $value, $offer);
     }
 
     private function add_images(array $images, DOMNode $offer)
@@ -113,7 +105,7 @@ class XMLGenerator
         }
     }
 
-    public function add_props(array $props, DOMNode &$offer)
+    private function add_props(array $props, DOMNode &$offer)
     {
         $properties = $this->add_elem('props', null, $offer);
         foreach ($props as $id => $property) {
@@ -127,15 +119,6 @@ class XMLGenerator
         $this->add_attr('id', $id, $prop);
         $this->add_attr('name', $property['name'], $prop);
     }
-
-    public function add_category_prop(array|string $sections, DOMNode &$offer){
-        $value = implode(';', $sections);
-        $this->add_elem('category', $value, $offer);
-    }
-
-    
-
-    
 
     public function save_xml(string $filename)
     {
